49454f0c050b21552249d78c6b68996f3ace9c77
Matija K <matijaleader5@gmail.com>
2020-04-13 16:40:52 +0200

    Initial commit

---
+++ QA/Models/Korisnik.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace QA.Models
+{
+    [Table("korisnik")]
+    public class Korisnik
+    {
+        [Key]
+        public int id { get; set; }
+        [StringLength(15, MinimumLength = 3, ErrorMessage = "{0} mora biti duljine minimalno {2} a maksimalno {1} znakova")]
+        [Required(ErrorMessage = "{0} je obavezno polje!")]
+        [Display(Name = "Korisničko ime")]
+        public string korisnickoIme { get; set; }
+        [Display(Name = "Lozinka")]
+        [Required(ErrorMessage = "{0} je obavezno polje!")]
+        public string lozinka { get; set; }
+        [StringLength(35, MinimumLength = 10, ErrorMessage = "{0} mora biti duljine minimalno {2} a maksimalno {1} znakova")]
+        [Required(ErrorMessage = "{0} je obavezno polje!")]
+        [Display(Name = "E-mail adresa")]
+        public string emailAdresa { get; set; }
+
+    }
+}
